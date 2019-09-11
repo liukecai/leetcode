381198ca34fcb07f1b4f4f17d0a110616ae0de2b
liukecai <liukecai8@hotmail.com>
2019-09-11 14:13:56 +0800

    Reverse partial linked list between index (m, n), pass

--- src/cn/edu/kust/easy/ReverseLinkedList.java
+++ src/cn/edu/kust/easy/ReverseLinkedList.java
@@ -4,10 +4,14 @@ import cn.edu.kust.util.ListNode;
 
 /**
  *
+ * 92. Reverse Linked List II
+ * pass
+ *
  * 206. Reverse Linked List
  * pass
  */
 public class ReverseLinkedList {
+    // 206
     public ListNode reverseList(ListNode head) {
         if (head == null)
             return null;
@@ -30,15 +34,39 @@ public class ReverseLinkedList {
         return p;
     }
 
+    // 92
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null)
+            return head;
+
+        ListNode reverseList = new ListNode(-1);
+        reverseList.next = head;
+
+        ListNode pre = reverseList;
+        for(int i = 1; i < m; i++)
+            pre = pre.next;
+
+        ListNode curr = pre.next;
+        for(int i = m; i < n; i++) {
+            ListNode next = curr.next;
+            curr.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return reverseList.next;
+    }
+
     public static void main(String[] args) {
         ListNode list = new ListNode(1);
         ListNode next = list;
         int i = 2;
-        while (i < 8) {
+        while (i < 20) {
             next.next = new ListNode(i);
             next = next.next;
             i++;
         }
+
+        System.out.println("====================Reverse all list====================");
 
         ListNode.printList(list);
 
@@ -46,5 +74,11 @@ public class ReverseLinkedList {
         ListNode reversed = reverse.reverseList(list);
 
         ListNode.printList(reversed);
+
+        System.out.println("====================Reverse partial list====================");
+
+        ListNode.printList(reversed);
+        ListNode reversedPartial = reverse.reverseBetween(reversed,8,15);
+        ListNode.printList(reversedPartial);
     }
 }
