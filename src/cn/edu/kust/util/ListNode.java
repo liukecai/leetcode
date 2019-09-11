9b951e18b7cb2152cc0dfa1c4de5b91ce590daf2
liukecai <liukecai8@hotmail.com>
2019-09-11 13:57:55 +0800

    Add method for print list

--- src/cn/edu/kust/util/ListNode.java
+++ src/cn/edu/kust/util/ListNode.java
@@ -4,4 +4,15 @@ public class ListNode {
     public int val;
     public ListNode next;
     public ListNode(int x) { val = x; }
+
+    public static void printList(ListNode list) {
+        ListNode next = list;
+        while (next.next != null) {
+            System.out.print(next.val);
+            System.out.print("->");
+            next = next.next;
+        }
+        System.out.print(next.val);
+        System.out.println("->NULL");
+    }
 }
